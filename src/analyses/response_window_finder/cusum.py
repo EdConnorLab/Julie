437bed083694434bd7ec3aecbb2e0624b2c9a34d
Julie <hwangjy90@gmail.com>
2025-03-10 18:07:25 -0400

    cusum_finalized

--- src/analyses/response_window_finder/cusum.py
+++ src/analyses/response_window_finder/cusum.py
@@ -20,7 +20,6 @@ def cusum(data, mu, k, h):
     for t in range(1, len(data)):
         cusum_pos[t] = max(0, cusum_pos[t - 1] + data[t] - mu - k)
         cusum_neg[t] = max(0, cusum_neg[t - 1] + mu - data[t] - k)
-        # print(f'cusum_pos: {cusum_pos[t]}, cusum_neg: {cusum_neg[t]}')
         if cusum_pos[t] > h or cusum_neg[t] > h:
             change_points.append(t)
     return cusum_pos, cusum_neg, change_points
@@ -29,13 +28,6 @@ def cusum(data, mu, k, h):
 def extract_consecutive_ranges(numbers):
     """
     Extracts ranges of consecutive numbers from a list of numbers.
-
-    Parameters:
-    numbers (list): A list of integers.
-
-    Returns:
-    list: A list of tuples, where each tuple contains the first and last consecutive integers from the input list,
-          excluding ranges where the start and end are the same.
     """
     result = []
     if len(numbers) > 0:
@@ -57,19 +49,19 @@ def extract_consecutive_ranges(numbers):
 def find_corresponding_values_for_index_ranges(index_ranges, values):
     """
     Extracts elements from a list based on the start and end indices provided in window_index_ranges.
-
-    Parameters:
-    window_index_ranges (list): A list of tuples with start and end indices.
-    time (list): A list from which elements are extracted based on the indices.
-
-    Returns:
-    list: A list of tuples with elements from the values list based on the given ranges.
     """
     extracted_values = []
     for start, end in index_ranges:
         if start <= len(values) and end < len(values):
             extracted_values.append((values[start], values[end]))
     return extracted_values
+
+def find_all_corresponding_values_falling_within_index_ranges(index_ranges, values):
+    all_corresponding_values = []
+    for start, end in index_ranges:
+        if start <= len(values) and end < len(values):
+            all_corresponding_values.append(values[start:end+1])
+    return all_corresponding_values
 
 def fill_missing_ones(numbers):
     if not numbers:
@@ -93,10 +85,7 @@ def compute_total_sum_of_spikes(raw_data, monkeys, channels, chunk_size):
 
     return spike_counts
 
-def compute_sum_across_monkeys(raw_data, monkeys, channels, chunk_size):
-    spike_counts = get_spike_counts_for_time_chunks(monkeys, raw_data, channels, chunk_size)
 
-# Min-Max Scaling
 def min_max_scale(data):
     min_val = np.min(data)
     max_val = np.max(data)
@@ -104,7 +93,10 @@ def min_max_scale(data):
     return scaled_data
 
 def z_score(data):
-    return (data - np.mean(data)) / np.std(data)
+    if np.std(data) == 0:
+        return np.zeros(len(data))
+    else:
+        return (data - np.mean(data)) / np.std(data)
 
 def plot_spike_count_with_response_windows(time, normalized_data, data, cusum_pos, cusum_neg, change_points, threshold):
     # Plotting
@@ -118,17 +110,17 @@ def plot_spike_count_with_response_windows(time, normalized_data, data, cusum_po
         plt.plot(time[:len(data)], data, label='Data')
     plt.plot(time[:len(data)], cusum_pos, label='CUSUM+', linestyle='--')
     plt.plot(time[:len(data)], cusum_neg, label='CUSUM-', linestyle='--')
-    # plt.scatter(t_values_at_change_points, y_values_at_change_points, color='red', zorder=5)
+    plt.scatter(t_values_at_change_points, y_values_at_change_points, color='red', zorder=5)
     plt.axhline(y=threshold, color='green', linestyle='--', label='Threshold')
     # Using your function to get consecutive ranges
     consecutive_ranges = extract_consecutive_ranges(change_points)
     overall_max = np.maximum.reduce([data, cusum_pos, cusum_neg])
     # max_of_normalized_data = np.maximum.reduce([normalized_data, cusum_pos, cusum_neg])
     # Shading windows
-    # for start, end in consecutive_ranges:
-    #     plt.fill_betweenx([0, max(overall_max)], time[start], time[end], color='red', alpha=0.4)
+    for start, end in consecutive_ranges:
+        plt.fill_betweenx([0, max(overall_max)], time[start], time[end], color='red', alpha=0.4)
 
-    # plt.title(f'{date_only} Round {round_no} {index} --- windows based on CUSUM algorithm')
+    plt.title(f'{date_only} Round {round_no} {index} --- windows based on CUSUM algorithm')
     plt.xlabel('Time')
     plt.ylabel('Value')
     plt.legend()
@@ -140,49 +132,65 @@ if __name__ == '__main__':
     del zombies[6]
     del zombies[-1]
 
-    recording_metadata = RecordingMetadataReader().get_raw_data()
-    all_rounds = recording_metadata.parse("AllRounds")
-
     time_chunk_size = 0.05  # in sec
     rounded_time = np.round(np.arange(time_chunk_size, 3.50, time_chunk_size), 2)
-
 
     response_window_test = pd.read_excel('response_window_algorithm_validation_test.xlsx')
     for _, row in response_window_test.iterrows():
         date = str(row['Date'])
         round_no = row['Round No.']
         date_only = row['Date'].strftime('%Y-%m-%d')
-        channels = row['Cell']
-        channels_to_read = convert_to_enum(channels)
+        channels_to_read = convert_to_enum(row['Cell'])
         raw_unsorted_data, _, sorted_data = get_raw_data_and_channels_from_files(date, round_no)
         spike_counts = compute_total_sum_of_spikes(raw_unsorted_data, zombies, [channels_to_read], time_chunk_size)
 
         spike_counts['response_windows'] = None
         for index, r in spike_counts.iterrows():
-            total_sum_data = r['total_sum']
-            std_dev = np.std(total_sum_data)
-            if std_dev > 0:
-                mean = np.mean(total_sum_data) # baseline mean
-                normalized_data = (total_sum_data - mean) / std_dev
-                max_data = max(total_sum_data)
+            data = r['total_sum']
+            normalized_data = z_score(data)
+            # Parameters
+            k = 1.1  # sensitivity parameter
+            h = 1.1 # threshold
+            cusum_pos, cusum_neg, change_points = cusum(normalized_data, 0, k, h)
+            windows = extract_consecutive_ranges(change_points)
+            time_windows = find_corresponding_values_for_index_ranges(windows, rounded_time)
+            if len(time_windows) > 0:
+                print(f"---------------- {date_only} round no. {round_no} {index}----------------")
+                print(time_windows)
+            # Plotting
+            y_values_at_change_points = [data[i] for i in change_points]
+            t_values_at_change_points = [rounded_time[i] for i in change_points]
 
-                # normalized_data = min_max_scale(total_sum_data)
-                # max_data = max(normalized_data)
-                # print(normalized_data)
-                # Parameters
-                k = 0.8  # sensitivity parameter
-                h = 0.5 # threshold
+            plt.figure(figsize=(12, 6))
+            # Using your function to get consecutive ranges
+            consecutive_ranges = extract_consecutive_ranges(change_points)
+            overall_max_with_data = np.maximum.reduce([data, cusum_pos, cusum_neg])
+            overall_max_with_norm_data = np.maximum.reduce([normalized_data, cusum_pos, cusum_neg])
+            # if normalized_data is not None:
+            #     plt.plot(rounded_time[:len(normalized_data)], normalized_data, label='Normalized Data')
+            #     for start, end in consecutive_ranges:
+            #         plt.fill_betweenx([0, max(overall_max_with_norm_data)], rounded_time[start], rounded_time[end], color='red',
+            #                           alpha=0.4)
 
-                cusum_pos, cusum_neg, change_points = cusum(total_sum_data, max_data, k, h)
-                windows = extract_consecutive_ranges(change_points)
-                time_windows = find_corresponding_values_for_index_ranges(windows, rounded_time)
-                if len(time_windows) > 0:
-                    print(f"---------------- {date_only} round no. {round_no} {index}----------------")
-                    # print(windows)
-                    print(time_windows)
+            if data is not None:
+                plt.plot(rounded_time[:len(data)], data, label='Data')
+                for start, end in consecutive_ranges:
+                    plt.fill_betweenx([0, max(overall_max_with_data)], rounded_time[start], rounded_time[end], color='red',
+                                      alpha=0.4)
+                plt.scatter(t_values_at_change_points, y_values_at_change_points, color='red', zorder=5)
 
-                # Plotting
-                plot_spike_count_with_response_windows(rounded_time, normalized_data, total_sum_data, cusum_pos, cusum_neg, change_points, h)
+            plt.plot(rounded_time[:len(data)], cusum_pos, label='CUSUM+', linestyle='--')
+            plt.plot(rounded_time[:len(data)], cusum_neg, label='CUSUM-', linestyle='--')
+            plt.axhline(y=h, color='green', linestyle='--', label='Threshold')
+
+
+
+            plt.title(f'{date_only} Round {round_no} {index} --- windows based on CUSUM algorithm')
+            plt.xlabel('Time')
+            plt.ylabel('Value')
+            plt.legend()
+            plt.show()
+
     '''
     results = []
     for _, row in all_rounds.iterrows():
