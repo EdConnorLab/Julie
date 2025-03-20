6924f2df5db43367824f5b0953a253babcffde69
Julie <hwangjy90@gmail.com>
2025-03-20 12:40:00 -0400

    simple_linear_regression.py organized a little

--- src/analyses/linear_regression/simple_linear_regression.py
+++ src/analyses/linear_regression/simple_linear_regression.py
@@ -4,7 +4,9 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import explained_variance_score
 from monkey_names import Zombies, BestFrans
+from recording_metadata_reader import RecordingMetadataReader
 from spike_count import get_spike_count_for_single_neuron_with_time_window
+
 
 def run_linear_regression_using_sklearn(x, y):
     x = np.array(x).reshape(-1,1)
@@ -37,6 +39,44 @@ def run_linear_regression_manually(x, y):
     r_sq = explained_variance_score(y, ypred)
     return b_1_ed, b_0_ed, r_sq
 
+def run_linear_regression_analysis(spike_counts, behavior_table, behavior_name):
+    results = []
+    for index, row in spike_counts.iterrows():
+        date = row['Date']
+        round_no = row['Round No.']
+        time_window = row['Time Window']
+
+        for sourcemonkey in range(len(zombies)):
+            exclude_columns = ['Date', 'Round No.', 'Time Window']
+            if sourcemonkey == subject_monkey_index:
+                continue
+            else:
+                behavior_table_arr = behavior_table[sourcemonkey, :]
+                y = np.delete(behavior_table_arr, [sourcemonkey, subject_monkey_index])
+                exclude_columns.append(zombies[sourcemonkey])
+                x = [value for key, value in row.items() if
+                     key not in exclude_columns and isinstance(value, (int, float))]
+            coeff, intercept, r_squared = run_linear_regression_using_sklearn(x, y)
+            if r_squared > 0.25:
+                print("")
+                print(
+                    f"-------- Linear Regression Results for {date} Round No.{round_no} {time_window} {index} -------")
+                print(
+                    f"------------------------------------{zombies[sourcemonkey]}--------------------------------------")
+                print(f"r-squared: {r_squared}")
+                results.append({
+                    'Date': date,
+                    'Round No.': round_no,
+                    'Cell': str(index),
+                    'Time Window': time_window,
+                    'Behavior': behavior_name,
+                    'Source_Monkey': zombies[sourcemonkey],
+                    'R-squared': r_squared
+                })
+    results_df = pd.DataFrame(results)
+    print(results_df)
+    results_df.to_excel(behavior_name + '.xlsx')
+    return results_df
 
 if __name__ == "__main__":
     # Zombies
@@ -62,7 +102,10 @@ if __name__ == "__main__":
     zombies_agonism_from = zombies_agonism_to.T
 
     # Cells
-    cells_with_windows = pd.read_excel("/home/connorlab/Documents/GitHub/Julie/src/analyses/response_window_finder/CUSUM_window_cells_ANOVA_passed.xlsx")
+    cusum_anova_passed_cells = pd.read_excel("/home/connorlab/Documents/GitHub/Julie/Cortana/CUSUM and ANOVA/Zombies_CUSUM_window_cells_ANOVA_passed.xlsx")
+    ed_anova_passed_cells = pd.read_excel("/home/connorlab/Documents/GitHub/Julie/Cortana/Ed and ANOVA/Ed_window_cells_ANOVA_passed.xlsx")
+    cells_with_windows = pd.concat([cusum_anova_passed_cells,ed_anova_passed_cells], ignore_index=True)
+    cells_with_windows['Date'] = pd.to_datetime(cells_with_windows['Date']).dt.date
     all_spike_counts = get_spike_count_for_single_neuron_with_time_window(cells_with_windows)
     all_spike_counts.columns = all_spike_counts.columns.astype(str)
     subject_monkey_index = 6
@@ -70,43 +113,14 @@ if __name__ == "__main__":
     zombies_without_subject_monkey = [item for item in zombies if item != subject_monkey]
     experimental_session_details = ['Date', 'Round No.', 'Time Window']
     zombies_spike_counts = all_spike_counts[zombies_without_subject_monkey + experimental_session_details]
-    print('AFFILIATION TO ANALYSIS')
     # get mean of spike counts
     zombies_spike_counts[zombies_without_subject_monkey] = zombies_spike_counts[zombies_without_subject_monkey].map(lambda x: sum(x) / len(x) if x else None)
-
-    count = 0
-    count_r_squared = 0
-    for index, row in zombies_spike_counts.iterrows():
-        date = row['Date']
-        round_no = row['Round No.']
-        time_window = row['Time Window']
-
-        for sourcemonkey in range(len(zombies)):
-            exclude_columns = ['Date', 'Round No.', 'Time Window']
-            if sourcemonkey == subject_monkey_index:
-                continue
-            else:
-                zombies_affiliation_to_arr = zombies_affiliation_to[sourcemonkey, :]
-                y = np.delete(zombies_affiliation_to_arr, [sourcemonkey, subject_monkey_index])
-                exclude_columns.append(zombies[sourcemonkey])
-                x = [value for key, value in row.items() if
-                               key not in exclude_columns and isinstance(value, (int, float))]
-            # print(f"x: {x}")
-            # print(f"y: {y}")
-            coeff, intercept, r_squared = run_linear_regression_using_sklearn(x, y)
-            manual_coeff, manual_intercept, manual_r_squared = run_linear_regression_manually(x, y)
-            if r_squared > 0.25:
-                print("")
-                print(f"-------- Linear Regression Results for {date} Round No.{round_no} {time_window} {index} -------")
-                print(f"------------------------------------{zombies[sourcemonkey]}--------------------------------------")
-                print(f"r^2 -- sklearn, ed's: {r_squared}, {manual_r_squared}")
-                print("")
-                count +=1
-            if r_squared > 0.5:
-                count_r_squared +=1
-    print(count)
-    print(count_r_squared)
-
+    run_linear_regression_analysis(zombies_spike_counts, zombies_affiliation_to, "AffliationTo")
+    run_linear_regression_analysis(zombies_spike_counts, zombies_affiliation_from, "AffliationFrom")
+    run_linear_regression_analysis(zombies_spike_counts, zombies_submission_to, "SubmissionTo")
+    run_linear_regression_analysis(zombies_spike_counts, zombies_submission_from, "SubmissionFrom")
+    run_linear_regression_analysis(zombies_spike_counts, zombies_agonism_to, "AgonismTo")
+    run_linear_regression_analysis(zombies_spike_counts, zombies_agonism_from, "AgonismFrom")
 
 
 '''an example to run for testing -- r squared has to be around 0.5092734647876507'''
