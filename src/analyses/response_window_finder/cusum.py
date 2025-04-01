28657874842cc529cc8adce9a1a5bdd255c33111
Julie <hwangjy90@gmail.com>
2025-04-01 18:11:37 -0400

    latest cusum stuff

--- src/analyses/response_window_finder/cusum.py
+++ src/analyses/response_window_finder/cusum.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
     time_chunk_size = 0.05  # in sec
     rounded_time = np.round(np.arange(time_chunk_size, 3.50, time_chunk_size), 2)
-    '''
+
     # response_window_test = pd.read_excel('response_window_algorithm_validation_test.xlsx')
     prelim = get_metadata_for_preliminary_analysis()
     shuffled_df = prelim.sample(frac=1, random_state=42)
@@ -120,18 +120,26 @@ if __name__ == '__main__':
         round_no = row['Round No.']
         date_only = row['Date'].strftime('%Y-%m-%d')
         # channels_to_read = convert_to_enum(row['Cell'])
+        # date = "2023-10-04"
+        # round_no = 3
         raw_unsorted_data, valid_channels, sorted_data = get_raw_data_and_channels_from_files(date, round_no)
+        # valid_channels = [Channel.C_002]
         spike_counts_unsorted_data = compute_total_sum_of_spikes(raw_unsorted_data, zombies, valid_channels, time_chunk_size)
         # if sorted_data is not None:
         #     print(f"sorted data exists for {date}, {round_no}")
         #     spike_counts_sorted_data = compute_total_sum_of_spikes(sorted_data, zombies, valid_channels, time_chunk_size)
         for index, r in spike_counts_unsorted_data.iterrows():
             data = r['total_sum']
+            #raw_filtered = gaussian_filter1d(data, sigma = 0.8)
             normalized_data = z_score(data)
             # Parameters
             k = 0.9  # sensitivity parameter
             h = 1 # threshold
-            cusum_pos, cusum_neg, change_points = cusum(normalized_data, 0, k, h)
+            threshold = 0.5
+            #filtered_data= gaussian_filter1d(normalized_data, sigma=0.8)
+
+            change_points = simple_thresholding(normalized_data, threshold)
+            # cusum_pos, cusum_neg, change_points = cusum(normalized_data, 0, k, h)
             windows = extract_consecutive_ranges(change_points)
             time_windows = find_corresponding_values_for_index_ranges(windows, rounded_time)
             if len(time_windows) > 0:
@@ -141,32 +149,34 @@ if __name__ == '__main__':
             y_values_at_change_points = [data[i] for i in change_points]
             t_values_at_change_points = [rounded_time[i] for i in change_points]
 
-            # plt.figure(figsize=(12, 6))
-            # consecutive_ranges = extract_consecutive_ranges(change_points)
+            plt.figure(figsize=(12, 6))
+            consecutive_ranges = extract_consecutive_ranges(change_points)
+            overall_max_for_simple_thresholding = np.maximum.reduce([normalized_data, data])
             # overall_max_with_data = np.maximum.reduce([data, cusum_pos, cusum_neg])
             # overall_max_with_norm_data = np.maximum.reduce([normalized_data, cusum_pos, cusum_neg])
-            # if normalized_data is not None:
-            #     plt.plot(rounded_time[:len(normalized_data)], normalized_data, label='Normalized Data')
-            #     for start, end in consecutive_ranges:
-            #         plt.fill_betweenx([0, max(overall_max_with_norm_data)], rounded_time[start], rounded_time[end], color='red',
-            #                           alpha=0.4)
+            if normalized_data is not None:
+                plt.plot(rounded_time[:len(normalized_data)], normalized_data, label='Normalized Data')
+                for start, end in consecutive_ranges:
+                    plt.fill_betweenx([0, max(overall_max_for_simple_thresholding)], rounded_time[start], rounded_time[end], color='red',
+                                      alpha=0.4)
 
-            # if data is not None:
-            #     plt.plot(rounded_time[:len(data)], data, label='Data')
-            #     for start, end in consecutive_ranges:
-            #         plt.fill_betweenx([0, max(overall_max_with_data)], rounded_time[start], rounded_time[end], color='red',
-            #                           alpha=0.4)
-            #     plt.scatter(t_values_at_change_points, y_values_at_change_points, color='red', zorder=5)
+            if data is not None:
+                plt.plot(rounded_time[:len(data)], data, label='Data')
+                for start, end in consecutive_ranges:
+                    plt.fill_betweenx([0, max(overall_max_for_simple_thresholding)], rounded_time[start], rounded_time[end], color='red',
+                                      alpha=0.4)
+                plt.scatter(t_values_at_change_points, y_values_at_change_points, color='red', zorder=5)
 
             # plt.plot(rounded_time[:len(data)], cusum_pos, label='CUSUM+', linestyle='--')
             # plt.plot(rounded_time[:len(data)], cusum_neg, label='CUSUM-', linestyle='--')
-            # plt.axhline(y=h, color='green', linestyle='--', label='Threshold')
-            #
-            # plt.title(f'{date_only} Round {round_no} {index} --- windows based on CUSUM algorithm')
-            # plt.xlabel('Time')
-            # plt.ylabel('Value')
-            # plt.legend()
-            # plt.show()
+            plt.axhline(y=threshold, color='green', linestyle='--', label='Threshold')
+
+            plt.title(f'{date_only} Round {round_no} {index} --- windows based on CUSUM algorithm')
+            plt.xlabel('Time')
+            plt.ylabel('Value')
+            plt.legend()
+            # plt.savefig("hi")
+            plt.show()
 
             if len(time_windows) > 0:
                 results.append({
@@ -176,35 +186,36 @@ if __name__ == '__main__':
                     'Time Window': time_windows
                 })
 
-    results_df = pd.DataFrame(results)
-    results_sorted = results_df.sort_values(by=['Date', 'Round No.', 'Cell'])
-    results_expanded = results_sorted.explode('Time Window')
-    results_expanded.to_excel('windows.xlsx')
+    # results_df = pd.DataFrame(results)
+    # results_sorted = results_df.sort_values(by=['Date', 'Round No.', 'Cell'])
+    # results_expanded = results_sorted.explode('Time Window')
+    # results_expanded.to_excel('windows.xlsx')
     '''
     # Date Created: 2025-01-29
     # ANOVA for windows found from cusum algorithm
-    results_expanded = pd.read_excel("/home/connorlab/Documents/GitHub/Julie/Cortana/CUSUM and ANOVA/Zombies_CUSUM_window_cells.xlsx")
+    results_expanded = pd.read_excel("/home/connorlab/Documents/GitHub/Julie/Cortana/CUSUM and ANOVA/CUSUM_window_cells.xlsx")
     results_expanded['Time Window'] = results_expanded['Time Window'].apply(
         lambda s: tuple(int(float(num) * 1000) for num in s.strip('()').split(',')))
     # results_expanded['Time Window'] = results_expanded['Time Window'].apply(
     #     lambda t: tuple(int(num * 1000) for num in t))
 
-    print(
-        "--------------------------------------------- cusum windows ----------------------------------------------------------")
-    print(results_expanded)
-
-    cusum_spike_count = get_spike_count_for_single_neuron_with_time_window(results_expanded)
-    print(cusum_spike_count)
-
-    zombies_columns = [col for col in zombies if col in cusum_spike_count.columns]
-    additional_columns = ['Date', 'Round No.', 'Time Window']
-    zombies_cusum_spike_count = cusum_spike_count[zombies_columns + additional_columns]
-    cusum_anova_results, cusum_sig_results = perform_anova_on_dataframe_rows_for_time_windowed(
-        zombies_cusum_spike_count)
-
-    print('------------------------------------ cusum window results -----------------------------------')
-    # print(cusum_anova_results)
-    print(cusum_sig_results)
-    print(cusum_sig_results.shape)
-    cusum_anova_results.to_excel('CUSUM_ANOVA_results.xlsx')
-    cusum_sig_results.to_excel('CUSUM_window_cells_ANOVA_passed.xlsx')
+    # print(
+    #     "--------------------------------------------- cusum windows ----------------------------------------------------------")
+    # print(results_expanded)
+    # 
+    # cusum_spike_count = get_spike_count_for_single_neuron_with_time_window(results_expanded)
+    # print(cusum_spike_count)
+    # 
+    # zombies_columns = [col for col in zombies if col in cusum_spike_count.columns]
+    # additional_columns = ['Date', 'Round No.', 'Time Window']
+    # zombies_cusum_spike_count = cusum_spike_count[zombies_columns + additional_columns]
+    # cusum_anova_results, cusum_sig_results = perform_anova_on_dataframe_rows_for_time_windowed(
+    #     zombies_cusum_spike_count)
+    # 
+    # print('------------------------------------ cusum window results -----------------------------------')
+    # # print(cusum_anova_results)
+    # print(cusum_sig_results)
+    # print(cusum_sig_results.shape)
+    # cusum_anova_results.to_excel('CUSUM_ANOVA_results.xlsx')
+    # cusum_sig_results.to_excel('CUSUM_window_cells_ANOVA_passed.xlsx')
+    '''
