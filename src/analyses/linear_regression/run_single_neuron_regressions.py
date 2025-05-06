6c38ca8ee95e9743418448976e93be8f1f0a9284
Julie <hwangjy90@gmail.com>
2025-05-06 14:07:55 -0400

    re-running run_single_neuron_regressions.py (directional vector) for zombies after re-stitching

--- src/analyses/linear_regression/run_single_neuron_regressions.py
+++ src/analyses/linear_regression/run_single_neuron_regressions.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 from analyses.enums.monkey_names import get_monkeys_by_default_order
 from analyses.linear_regression.directional_behavioral_vector_analysis import run_directional_vector_linear_regression
+from analyses.spike_count import extract_spike_counts_from_cells
 from analyses.spike_rate import compute_mean_spike_rate_for_cells, compute_mean_spike_rate_for_windows
 
 
@@ -25,8 +26,9 @@ def plot_single_neuron_profile(df, neuron_id):
     plt.show()
 
 def main():
-    monkey_group_name = "Zombies"
-    monkey_list = get_monkeys_by_default_order(monkey_group_name)
+    monkey_group = "Zombies"
+    subject_monkey_index = 6
+    monkey_list = get_monkeys_by_default_order(monkey_group)
     base_dir = '/home/connorlab/Documents/GitHub/Julie/social_data/zombies_social_data/'
     behavior_files = {
         "AffiliationTo": "zombies_feature_df_affiliation.xlsx",
@@ -42,28 +44,35 @@ def main():
         else pd.read_excel(os.path.join(base_dir, fname)).iloc[:, 1:].to_numpy()
         for name, fname in behavior_files.items()
     }
-    sig_neurons = pd.read_pickle('/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_cache/Zombies_significant_neurons_pANOVAorGLM_passed.pkl')
-    sig_windows = pd.read_pickle(
-        '/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_cache/Zombies_significant_windows_pANOVAorGLM_passed.pkl')
-    mean_spike_rate = compute_mean_spike_rate_for_cells(sig_neurons)
 
+    # Directional Linear Regression (OLS) on Significant Cells
+    sig_neurons = pd.read_pickle(f'/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_cache/{monkey_group}_significant_neurons_pANOVAorGLM_passed.pkl')
+    mean_spike_rate_neurons = compute_mean_spike_rate_for_cells(sig_neurons)
+    neuron_results = []
+    for name, mat in behavior_matrices.items():
+        neuron_results_df = run_directional_vector_linear_regression(
+            mean_spike_rate_neurons, mat, name, monkey_group, monkey_list, subject_monkey_index, use_spikerate=True
+        )
+        neuron_results.append(neuron_results_df)
+    all_neuron_results_df = pd.concat(neuron_results, ignore_index=True)
+    all_neuron_results_sorted = all_neuron_results_df.sort_values(by=['p_value','R-squared'], ascending=False)
+    all_neuron_results_sorted.to_pickle(f'/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_results/{monkey_group}_dir_ols_on_single_neurons.pkl')
+    print(all_neuron_results_sorted)
+
+    # Directional Linear Regression (OLS) on Significant Windows
+    sig_windows = pd.read_pickle(
+        f'/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_cache/{monkey_group}_significant_windows_pANOVAorGLM_passed.pkl')
     mean_spike_rate_windows = compute_mean_spike_rate_for_windows(sig_windows)
-    subject_monkey_index = 6
-    all_results = []
+    all_window_results = []
     for name, mat in behavior_matrices.items():
         results_df = run_directional_vector_linear_regression(
-            mean_spike_rate_windows, mat, name, monkey_group_name, monkey_list, subject_monkey_index, use_spikerate=True
+            mean_spike_rate_windows, mat, name, monkey_group, monkey_list, subject_monkey_index, use_spikerate=True
         )
-        all_results.append(results_df)
-    all_results_df = pd.concat(all_results, ignore_index=True)
-    all_results_sorted = all_results_df.sort_values(by=['p_value','R-squared'], ascending=False)
-    significant_results = all_results_sorted[all_results_sorted['p_value'] < 0.05]
-    # significant_results.to_excel('/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_results/Zombies_dir_linreg_on_neurons_only_significant.xlsx')
-    all_results_sorted.to_pickle('/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_results/Zombies_dir_linreg_on_windows.pkl')
-    # print(all_results_sorted)
-    # significant_results = pd.read_pickle('/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_results/directional_linear_regression_on_single_neurons.pkl')
-    # significant_results = significant_results[significant_results['p_value'] < 0.05]
-
+        all_window_results.append(results_df)
+    all_window_results_df = pd.concat(all_window_results, ignore_index=True)
+    all_window_results_sorted = all_window_results_df.sort_values(by=['p_value','R-squared'], ascending=False)
+    all_window_results_sorted.to_pickle(f'/home/connorlab/Documents/GitHub/Julie/Cortana/analysis_results/{monkey_group}_dir_ols_on_windows.pkl')
+    print(all_window_results_sorted)
 
 
 if __name__ == "__main__":
