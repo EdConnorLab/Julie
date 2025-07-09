594d52d1d1f1d06efcd7d28c2fc0d8c03c77effb
mohrabi <m.rabiei.gh@gmail.com>
2025-07-09 12:17:29 -0400

    deleting commented out lines

--- src/analyses/linear_regression/behavior_vector_linear_regression.py
+++ src/analyses/linear_regression/behavior_vector_linear_regression.py
@@ -686,11 +686,6 @@ def run_rsa_analysis(spike_df, behavior_matrix, behavior_name, monkey_list, subj
         index=filtered_monkeys,
         columns=filtered_monkeys
     )
-    # neural_rsm = pd.DataFrame(
-    #     np.corrcoef(neural_matrix.T),
-    #     index=filtered_monkeys,
-    #     columns=filtered_monkeys
-    # )
 
     # Social RSM
     social_rsm = pd.DataFrame(
@@ -698,12 +693,6 @@ def run_rsa_analysis(spike_df, behavior_matrix, behavior_name, monkey_list, subj
         index=filtered_monkeys,
         columns=filtered_monkeys
     )
-
-    # social_rsm = pd.DataFrame(
-    #     np.corrcoef(behavior_matrix.T),
-    #     index=filtered_monkeys,
-    #     columns=filtered_monkeys
-    # )
 
     # Flatten upper triangle for correlation
     mask = np.triu(np.ones_like(neural_rsm), k=1).astype(bool)
