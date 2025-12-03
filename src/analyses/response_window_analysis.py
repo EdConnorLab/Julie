9cc75d5d84951149805159973924d8ef9a5156b4
Julie <hwangjy90@gmail.com>
2025-12-02 20:31:11 -0500

    added permutation kruskal wallis by window in response_window_analysis.py

--- src/analyses/response_window_analysis.py
+++ src/analyses/response_window_analysis.py
@@ -4,9 +4,84 @@ import statsmodels.formula.api as smf
 import statsmodels.api as sm
 from statsmodels.stats.multitest import multipletests
 
-from analyses.single_neuron_analysis import plot_permutation_anova_distribution
-from analyses.statistical_tests import perform_statistical_test_on_dataframe_rows, permutation_anova_test
+from analyses.single_neuron_analysis import plot_permutation_anova_distribution, plot_permutation_kw_distribution
+from analyses.statistical_tests import perform_statistical_test_on_dataframe_rows, permutation_anova_test, \
+    permutation_kruskal_test
 
+
+def run_permutation_kruskal_wallis_by_window(df,
+                                    category_col='MonkeyName',
+                                    neuron_col='NeuronID',
+                                    count_col='SpikeCount',
+                                    window_start_col='WindowStart_ms',
+                                    window_end_col='WindowEnd_ms',
+                                    n_permutations=1000,
+                                    alpha=0.05,
+                                    plot=False):
+    """
+    Perform permutation Kruskal-Wallis for each NeuronID and time window combination.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Trial-level spike count data. Must include NeuronID, SpikeCount,
+        stimulus category column (e.g., MonkeyName), and window columns.
+    """
+    all_results = []
+
+    group_cols = [neuron_col, window_start_col, window_end_col]
+    grouped_df = df.groupby(group_cols)
+
+    for (neuron, win_start, win_end), sub_df in tqdm(grouped_df, desc="Running Permutation Kruskal-Wallis per (Neuron, Window)"):
+        grouped = sub_df.groupby(category_col)[count_col].apply(list)
+
+        # Skip if not enough groups
+        if len(grouped) < 2:
+            continue
+
+        test_input_df = pd.DataFrame([grouped])
+
+        results, total_significant, details = perform_statistical_test_on_dataframe_rows(
+            test_input_df,
+            test_func=permutation_kruskal_test,
+            num_permutations=n_permutations,
+            alpha=alpha
+        )
+
+        for result in results:
+            index, H_stat, p_value = result
+            all_results.append({
+                'NeuronID': neuron,
+                'WindowStart_ms': win_start,
+                'WindowEnd_ms': win_end,
+                'H-statistic': H_stat,
+                'p-value': p_value
+            })
+
+            if plot:
+                detail = details.get(index, {})
+                extras = detail.get('extras')
+                if extras:
+                    # print(extras)
+                    perm_H, = extras
+                    plot_permutation_kw_distribution(
+                        perm_h_stats=perm_H,
+                        observed_h_stat=detail['stat'],
+                        neuron_id=neuron,
+                        category_name=category_col,
+                        output_dir="permutation_kw_plots"
+                    )
+
+    results_df = pd.DataFrame(all_results)
+    significant_df = results_df[results_df['p-value'] < alpha]
+
+    print("\nAll Results:")
+    print(results_df)
+
+    print("\nSignificant windows (p < 0.05):")
+    print(significant_df)
+
+    return results_df, significant_df
 
 def run_permutation_anova_by_window(df,
                                     category_col='MonkeyName',
