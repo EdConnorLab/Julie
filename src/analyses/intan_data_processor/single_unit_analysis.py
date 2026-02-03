871154910c0e4e9445d66fe60678914cad6fa1b5
Julie <hwangjy90@gmail.com>
2026-02-03 16:10:57 -0500

    comment out matplotlib.use("Qt5Agg")

--- src/analyses/intan_data_processor/single_unit_analysis.py
+++ src/analyses/intan_data_processor/single_unit_analysis.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 from analyses.enums.monkey_names import get_monkeys_by_rank
 from compile.sorted_units_compilation import read_pickle
 
-matplotlib.use("Qt5Agg")
+# matplotlib.use("Qt5Agg")
 
 
 def main():
---
+++ src/test_remote.py
@@ -0,0 +1,5 @@
+import socket, sys, os
+
+print(socket.gethostname())
+print(sys.executable)
+print(os.getcwd())
